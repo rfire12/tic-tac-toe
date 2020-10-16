109f3d20561f4e9344f26443c63e3df4e62377a8
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-10-16 10:09:18 -0400

    Game works

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 import pygame
 import square
 
+# Setup
 pygame.init()
 window = pygame.display.set_mode((550, 550))
 pygame.display.set_caption('Tic Tac Toe')
 game_running = True
+message_render = pygame.font.Font(pygame.font.get_default_font(), 36)
+
+# Game details
 board = [square.Square(window, (25, 25, 150, 150)), square.Square(window, (200, 25, 150, 150)),
          square.Square(window, (375, 25, 150, 150)), square.Square(window, (25, 200, 150, 150)),
          square.Square(window, (200, 200, 150, 150)), square.Square(window, (375, 200, 150, 150)),
          square.Square(window, (25, 375, 150, 150)), square.Square(window, (200, 375, 150, 150)),
          square.Square(window, (375, 375, 150, 150))]
+
+winning_positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                     [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                     [0, 4, 8], [2, 4, 6]]
+
+current_player = 1
+
+
+def mark_square():
+    marked_valid_pos = False
+    for square_box in board:
+        if square_box.get_square().collidepoint(pos) and square_box.get_marked_by() is False:
+            square_box.mark(current_player)
+            marked_valid_pos = True
+
+    return marked_valid_pos
+
+
+def check_winning_positions():
+    for win_position in winning_positions:
+        first_square = board[win_position[0]].get_marked_by() == current_player
+        second_square = board[win_position[1]].get_marked_by() == current_player
+        third_square = board[win_position[2]].get_marked_by() == current_player
+
+        if first_square and second_square and third_square:
+            text_surface = message_render.render(f'Player {current_player} wins', True, (255, 255, 255))
+            window.blit(text_surface, (150, 250))
+
 
 while game_running:
     pygame.time.delay(100)
@@ -17,7 +49,18 @@ while game_running:
         if event.type == pygame.QUIT:
             game_running = False
 
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+
+            if mark_square():
+                check_winning_positions()
+                current_player = 1 if current_player == 2 else 2  # switch players
+
     pygame.display.update()
+
+
+
+
 pygame.quit()
 
 
--- square.py
+++ square.py
@@ -3,4 +3,19 @@ import pygame
 
 class Square:
     def __init__(self, window, position):
+        self.__window = window
+        self.__position = position
         self.__square = pygame.draw.rect(window, (194, 194, 194), position)
+        self.__marked_by = False
+
+    def get_square(self):
+        return self.__square
+
+    def mark(self, player):
+        if not self.__marked_by:
+            color = (50, 168, 82) if player == 1 else (252, 3, 3)
+            self.__square = pygame.draw.rect(self.__window, color, self.__position)
+            self.__marked_by = player
+
+    def get_marked_by(self):
+        return self.__marked_by
